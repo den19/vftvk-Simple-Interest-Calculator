b06b5c35f7cce080d3579f08c59172e9a8bd8f0b
Denis Kolesov <den.kolesov@gmail.com>
2021-07-06 19:19:52 +0300

    Fix Alert

--- script.js
+++ script.js
@@ -1,32 +1,65 @@
-function calc()
+/*
+Melissa Connors
+edX IBM Introduction to Cloud Development
+*/
+
+//Display slider value selected
+function rateDisplay()
 {
-    let principal = document.getElementById("interest").value;
+	var slider = document.getElementById("rate");
+	var output = document.getElementById("displayRate");
+	output.innerHTML = slider.value.concat("%");
+}
+
+//Populate the No. of Years drop-down
+function listYears()
+{
+	//List should have 1-10 years
+	for(var i=1; i<=10; i++)
+	{
+    		var select = document.getElementById("years");
+    		var option = document.createElement("OPTION");
+    		select.options.add(option);
+    		option.text = i;
+    		option.value = i;
+	}
+}
+
+//Calculate the interest
+function compute()
+{
+    	var principal = document.getElementById("principal").value;
+	//Validate the prinicpal amount
 	if (principal <= 0)
 	{
-   		alert("The Amount must be greater than zero.");
-   		document.getElementById("interest").focus();
-		document.getElementById("interest").click();
-   		return null;
-	}
-	let amount = document.getElementById("amount").value;	
-	let years = document.getElementById("years").value;
-	let interest = principal * years * amount / 100;
-	let currentDate = new Date();
-  	let currentYear = currentDate.getFullYear();
-	let futureYear = parseInt(currentYear) + parseInt(years);
 
-	let msg = "If you deposit <mark>" + amount + "</mark>," 
-	+ "<br/>at an interest rate of <mark>" + principal + "</mark>."
+   		alert("The Amount must be greater than zero.");
+
+   		document.getElementById("principal").focus();
+		document.getElementById("principal").click();
+
+   		return null;
+
+	}
+	
+	var rate = document.getElementById("rate").value;
+	var years = document.getElementById("years").value;
+	
+	var interest = principal * years * rate / 100;
+
+	//Get the current year to calcuate this year plus No. of Years
+	var currentDate = new Date();
+  	var currentYear = currentDate.getFullYear();
+	var futureYear = parseInt(currentYear) + parseInt(years);
+
+	//Final caluclation message
+	var finalMessage = "If you deposit <mark>" + principal + "</mark>," 
+	+ "<br/>at an interest rate of <mark>" + rate + "%</mark>."
 	+ "<br/>You will receive an amount of <mark>" + interest + "</mark>,"
 	+ "<br/>in the year <mark>" + futureYear + "</mark><br/>";  
-	document.getElementById("result").innerHTML = msg;
-	document.getElementById("interest").focus();
-}
+	document.getElementById("result").innerHTML = finalMessage;
 
-function showInterest()
-{
-	let interest = document.getElementById("interest");
-	let output = document.getElementById("rate");
-	output.innerHTML = interest.value.concat("%");
-}
+	//Put the focus back in the amount
+	document.getElementById("principal").focus();
 
+}
